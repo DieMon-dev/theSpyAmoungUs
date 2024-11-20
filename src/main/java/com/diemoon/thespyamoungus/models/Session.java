9b6969a4363b038a0fb9938cbc7f5e3a0b6f751a
Dmytro Nakutnii <72558879+DieMon-dev@users.noreply.github.com>
2024-11-20 22:46:37 +0100

    Start. User and session classes added

---
+++ src/main/java/com/diemoon/thespyamoungus/models/Session.java
@@ -0,0 +1,17 @@
+package com.diemoon.thespyamoungus.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Session {
+    private Integer sessionId;
+    private String sessionName;
+    private String sessionPassword;
+    private String sessionAdmin;
+    private String sessionStatus;
+    private String sessionUsers;
+    private Integer sessionMaxUsers;
+    private Integer sessionCurrentUsers;
+}
---
+++ src/test/java/com/diemoon/thespyamoungus/TheSpyAmoungUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diemoon.thespyamoungus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSpyAmoungUsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
