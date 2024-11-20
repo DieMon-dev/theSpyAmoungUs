f4b952d310d7302d4f2d9b52d9e15d9570b2043e
Dmytro Nakutnii <72558879+DieMon-dev@users.noreply.github.com>
2024-11-20 23:12:15 +0100

    blablabla

---
+++ src/main/java/com/diemoon/thespyamoungus/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.diemoon.thespyamoungus.controllers;
+
+import com.diemoon.thespyamoungus.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+}
---
+++ src/main/java/com/diemoon/thespyamoungus/services/UserService.java
@@ -0,0 +1,9 @@
+package com.diemoon.thespyamoungus.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
